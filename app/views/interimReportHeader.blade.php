f4735bf83cc8ad0a3f9f6801ee6a3729a6049127
Geoffrey <geoflen@yahoo.com>
2019-05-08 17:58:16 +0300

    interim report logo update

--- app/views/interimReportHeader.blade.php
+++ app/views/interimReportHeader.blade.php
@@ -8,32 +8,36 @@
 
 
     <table style="padding: 0px;" >
-        <thead>
+        
             <tr>
                 <td colspan="12"></td>
             </tr>
     </table>
     <table style="text-align:center;" >
             <tr>
-                <td colspan="12" style="text-align:center;">
+                 <td colspan="12" style="text-align:center;">
 
-                  {{ @HTML::image(Config::get('kblis.rhsp-logo'),  Config::get('kblis.country') . trans('messages.court-of-arms'), array('width' => '40px')) }} 
-                   </td>
-            </tr>
-            <tr>
-               
-                <td colspan="12" style="text-align:center;"><b>
-                    {{ strtoupper(Config::get('constants.MINISTRY')) }}<br>
-                    <span style="font-size:14px">
-                        {{ strtoupper(Config::get('constants.FACILITY_NAME')) }}<br>
-                    </span>
-                    
-                    {{Config::get('kblis.address-info')}}</b>
-                     {{Config::get('kblis.interim-report-name')}}
+                   {{ @HTML::image(Config::get('kblis.rhsp-logo'),  Config::get('kblis.country') . trans('messages.court-of-arms'), array('width' => '650px')) }}
                 </td>
             </tr>
-        </thead>
+            <tr><td colspan="12" style="text-align:center;">{{Config::get('kblis.interim-report-name')}}</td></tr>
+
+            
+        
     </table>
+
+    <br>
+    <br>
+    <table style="border-bottom: 1px solid #cecfd5; font-size:8px;font-family: 'Courier New',Courier;">
+    <tr>
+        <td width="15%"><strong>Patient ID</strong>:</td>
+        <td width="55%" style="text-align:left; ">{{ $patient->ulin}}</td>
+
+        <td width="15%"><strong>{{ trans('messages.report-date')}}</strong>:</td>
+        <td width="15%" style="text-align:left;">{{ date('d-m-Y') }}</td>
+
+    </tr>
+</table>
 
      <br>
     <br>
