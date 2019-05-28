882b4e2e959cce883744ab078e1a0972d93c7d24
Geoffrey <geoflen@yahoo.com>
2019-05-28 13:21:07 +0300

    date formatd-Y-M

--- app/models/Mypdf.php
+++ app/models/Mypdf.php
@@ -4,17 +4,17 @@ class Mypdf extends TCPDF {
 	//Pdf Header
 	Public function Header(){
 		if($this->page == 1){
-			$this->writeHTML(View::make('reportHeader'), true, false, true, false, '');
+			$this->writeHTML(View::make('finalReportHeader'), true, false, true, false, '');
 			$this->SetMargins(PDF_MARGIN_LEFT, 50, PDF_MARGIN_RIGHT);
 		}else {
-			$this->SetMargins(PDF_MARGIN_LEFT, 15, PDF_MARGIN_RIGHT);
+			$this->SetMargins(PDF_MARGIN_LEFT, 25, PDF_MARGIN_RIGHT);
 		}
 		
 	}
 
 	Public function Footer(){
 		$now = new DateTime();
-		$printTime = $now->format('Y-m-d H:i');
+		$printTime = $now->date_format('d-M-Y H:i');
 
 		//Position at 15mm at the bottom
 		$this->SetY(-15);
